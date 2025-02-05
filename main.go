e049a3bcf7eabe7d647c9737b173827380f9daa4
andi ahmad <andi.saputra@salt.co.id>
2025-02-05 16:49:08 +0700

    testing

--- main.go
+++ main.go
@@ -2,77 +2,21 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
-
-	"github.com/go-chi/chi/middleware"
-	"github.com/go-chi/chi/v5"
 )
 
-var rolesPermissions = map[string][]string{
-	"user":  {"read", "read-product"},
-	"admin": {"read", "write"},
-}
-
-func ACLMiddleware(permission []string) func(next http.Handler) http.Handler {
-	return func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			userRole := "user"
-
-			requiredPermissions := make(map[string]bool)
-			for _, p := range permission {
-				requiredPermissions[p] = true
-			}
-
-			userPermission, exist := rolesPermissions[userRole]
-			if !exist {
-				http.Error(w, "Unauthorized", http.StatusUnauthorized)
-				return
-			}
-
-			var authorized bool
-			for _, p := range userPermission {
-				fmt.Println("list permission", p)
-				if requiredPermissions[p] {
-					authorized = true
-					break
-				}
-			}
-
-			if !authorized {
-				http.Error(w, "Forbiden", http.StatusForbidden)
-				return
-			}
-
-			next.ServeHTTP(w, r)
-		})
-
-	}
-}
-
+// Fungsi ini yang akan di-export dan digunakan oleh Vercel
 func Handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
+	// Menulis respon HTTP
+	fmt.Fprintf(w, "Hello from Go on Vercel!")
 }
 
 func main() {
-	r := chi.NewRouter()
-	r.Use(middleware.RequestID)
-	r.Use(middleware.RealIP)
-	r.Use(middleware.Logger)
-	r.Use(middleware.Recoverer)
+	// Menentukan route untuk menangani semua request ke endpoint "/"
+	http.HandleFunc("/", Handler)
 
-	r.Get("/read", Handler)
-
-	r.With(ACLMiddleware([]string{"read-product"})).Get("/product", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Read product done"))
-	})
-
-	r.With(ACLMiddleware([]string{"write"})).Get("/write", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Write operation allowed"))
-	})
-
-	port := 9999
-	log.Printf("server running on port:%d", port)
-	http.ListenAndServe(":9999", r)
-
+	// Menggunakan port yang diberikan oleh Vercel
+	port := "3000"
+	http.ListenAndServe(":"+port, nil)
 }
+
