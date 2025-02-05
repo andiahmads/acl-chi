06dc10c51e316163a812e763c69ccafe7dd490cd
andi ahmad <andi.saputra@salt.co.id>
2025-02-05 16:51:16 +0700

    testing

--- main.go
+++ main.go
@@ -2,21 +2,81 @@ package main
 
 import (
 	"fmt"
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5"
 	"net/http"
+	"os"
 )
 
-// Fungsi ini yang akan di-export dan digunakan oleh Vercel
-func Handler(w http.ResponseWriter, r *http.Request) {
-	// Menulis respon HTTP
-	fmt.Fprintf(w, "Hello from Go on Vercel!")
+// ACLMiddleware to check permissions
+func ACLMiddleware(permission []string) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			userRole := "user"
+			requiredPermissions := make(map[string]bool)
+
+			for _, p := range permission {
+				requiredPermissions[p] = true
+			}
+
+			rolesPermissions := map[string][]string{
+				"user":  {"read", "read-product"},
+				"admin": {"read", "write"},
+			}
+
+			userPermission, exist := rolesPermissions[userRole]
+			if !exist {
+				http.Error(w, "Unauthorized", http.StatusUnauthorized)
+				return
+			}
+
+			var authorized bool
+			for _, p := range userPermission {
+				if requiredPermissions[p] {
+					authorized = true
+					break
+				}
+			}
+
+			if !authorized {
+				http.Error(w, "Forbidden", http.StatusForbidden)
+				return
+			}
+
+			next.ServeHTTP(w, r)
+		})
+	}
 }
 
-func main() {
-	// Menentukan route untuk menangani semua request ke endpoint "/"
-	http.HandleFunc("/", Handler)
+// main handler function
+func mainHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, Vercel!")
+}
 
-	// Menggunakan port yang diberikan oleh Vercel
-	port := "3000"
-	http.ListenAndServe(":"+port, nil)
+// Vercel entry point function
+func Handler(w http.ResponseWriter, r *http.Request) {
+	routes := chi.NewRouter()
+	routes.Use(middleware.RequestID)
+	routes.Use(middleware.RealIP)
+	routes.Use(middleware.Logger)
+	routes.Use(middleware.Recoverer)
+
+	routes.Get("/", mainHandler)
+
+	// Apply ACL middleware for routes with permission checks
+	routes.With(ACLMiddleware([]string{"read"})).Get("/read", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Read operation allowed"))
+	})
+
+	routes.With(ACLMiddleware([]string{"read-product"})).Get("/product", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Read product done"))
+	})
+
+	routes.With(ACLMiddleware([]string{"write"})).Get("/write", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Write operation allowed"))
+	})
+
+	http.Handle("/", routes)
+	http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 }
 
